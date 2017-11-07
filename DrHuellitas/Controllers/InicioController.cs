a8fac4b04cb572ae80568394e8842b6d8341bc1b
luisfrr <luisfrr27@gmail.com>
2017-11-07 14:10:11 -0600

    Proyecto MVC Dr. Huellitas

---
+++ DrHuellitas/Controllers/InicioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DrHuellitas.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult login2()
+        {
+            return View();
+        }
+    }
+}

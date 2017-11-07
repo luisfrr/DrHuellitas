b72a4de10792e85edbdb45a7893f291e71816a08
luisfrr <luisfrr27@gmail.com>
2017-11-07 17:53:19 -0600

    Se agrego login, estilos y scripts del login. Se agrego la carpeta BO y DAO Al igual que RegistroBO

---
+++ DrHuellitas/BO/RegistroBO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrHuellitas.BO
+{
+    public class RegistroBO
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string usuario { get; set; }
+        public string contraseña { get; set; }
+        public string nombre { get; set; }
+        public int id_tipo { get; set; }
+        public string rep_contraseña { get; set; }
+
+    }
+}

1da66430bb404d05a662c51d4f82d4b2ce43139b
luisfrr <luisfrr27@gmail.com>
2017-11-16 01:35:54 -0600

    CRUD casi listo, solo falta editar :c

--- DrHuellitas/Controllers/MasterAdmin/GestionesController.cs
+++ DrHuellitas/Controllers/MasterAdmin/GestionesController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DrHuellitas.BO;
+using DrHuellitas.DAO;
+using System.Data;
+using Newtonsoft.Json;
 
 namespace DrHuellitas.Controllers.MasterAdmin
 {
     public class GestionesController : Controller
     {
+        PaquetesDAO objPaqueteDAO = new PaquetesDAO();
+
         // GET: Gestiones
         public ActionResult Usuarios()
         {
@@ -32,6 +38,66 @@ namespace DrHuellitas.Controllers.MasterAdmin
         public ActionResult Paquetes()
         {
             return View();
+        }
+
+        public JsonResult ObtenerListaPaquetes()
+        {
+            List<PaquetesBO> PackList = objPaqueteDAO.ListPaquetes().ToList();
+            return Json(PackList, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ObtenerPaquete(int idPaquete)
+        {
+            List<PaquetesBO> PackList = objPaqueteDAO.ObtenerPaquete(idPaquete).ToList();
+            return Json(PackList, JsonRequestBehavior.AllowGet);
+        }
+
+        /*public JsonResult ObtenerPaquete(int idPaquete)
+        {
+            DataTable dt = objPaqueteDAO.ObtenerPaquete(idPaquete);
+            string value = string.Empty;
+            value = JsonConvert.SerializeObject(dt, Formatting.Indented, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+            return Json(value, JsonRequestBehavior.AllowGet);
+        }*/
+
+        public JsonResult GuardarPaquetes(PaquetesBO model)
+        {
+            var result = false;
+            try
+            {
+                if (model.id > 0)
+                {
+                    objPaqueteDAO.ModificarPaquete(model);
+                    result = true;
+                }
+                else
+                {
+                    objPaqueteDAO.AgregarPaquete(model);
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult EliminarPaquete(int idPaquete)
+        {
+            bool result = false;
+
+            int x = objPaqueteDAO.EliminarPaquete(idPaquete);
+            if (x != 0)
+            {
+                result = true;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Comentarios()
---
+++ DrHuellitas/DAO/PaquetesDAO.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using DrHuellitas.BO;
+
+namespace DrHuellitas.DAO
+{
+    public class PaquetesDAO
+    {
+        ConexionSQL con = new ConexionSQL();
+
+        public int AgregarPaquete(PaquetesBO objBO)
+        {
+            SqlCommand cmd = new SqlCommand("INSERT INTO Paquetes (nombre,precio,descripcion) VALUES(@nombre,@precio,@descripcion)");
+            cmd.Parameters.Add("@nombre", SqlDbType.VarChar).Value = objBO.nombre;
+            cmd.Parameters.Add("@precio", SqlDbType.Real).Value = objBO.precio;
+            cmd.Parameters.Add("@descripcion", SqlDbType.VarChar).Value = objBO.descripcion;
+
+            return con.EjecutarComando(cmd);
+        }
+
+        public int ModificarPaquete(PaquetesBO objBO)
+        {
+            SqlCommand cmd = new SqlCommand("UPDATE Paquetes SET nombre=@nombre,precio=@precio,descripcion=@precio WHERE id=@id");
+            cmd.Parameters.Add("@nombre", SqlDbType.VarChar).Value = objBO.nombre;
+            cmd.Parameters.Add("@precio", SqlDbType.Real).Value = objBO.precio;
+            cmd.Parameters.Add("@descripcion", SqlDbType.VarChar).Value = objBO.descripcion;
+            cmd.Parameters.Add("@id", SqlDbType.Int).Value = objBO.id;
+
+            return con.EjecutarComando(cmd);
+        }
+
+        public int EliminarPaquete(int idPaquete)
+        {
+            SqlCommand cmd = new SqlCommand("DELETE FROM Paquetes WHERE id=@id");
+            cmd.Parameters.Add("id", SqlDbType.Int).Value = idPaquete;
+
+            return con.EjecutarComando(cmd);
+        }
+
+        public List<PaquetesBO> ListPaquetes()
+        {
+            var paquetes = new List<PaquetesBO>();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Paquetes");
+
+            cmd.Connection = con.establecerConexion();
+            con.AbrirConexion();
+            var query = cmd;
+            using (var dr = query.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    var p = new BO.PaquetesBO
+                    {
+                        id = Convert.ToInt32(dr["id"]),
+                        nombre = dr["nombre"].ToString(),
+                        precio = Convert.ToDouble(dr["precio"].ToString()),
+                        descripcion = dr["descripcion"].ToString()
+                    };
+                    paquetes.Add(p);
+                }
+            }
+            return paquetes;
+        }
+
+        public List<PaquetesBO> ObtenerPaquete(int id)
+        {
+            var paquetes = new List<PaquetesBO>();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Paquetes WHERE id=@id");
+            cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+
+            cmd.Connection = con.establecerConexion();
+            con.AbrirConexion();
+            var query = cmd;
+            using (var dr = query.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    var p = new BO.PaquetesBO
+                    {
+                        id = Convert.ToInt32(dr["id"]),
+                        nombre = dr["nombre"].ToString(),
+                        precio = Convert.ToDouble(dr["precio"].ToString()),
+                        descripcion = dr["descripcion"].ToString()
+                    };
+                    paquetes.Add(p);
+                }
+            }
+            return paquetes;
+        }
+
+        /*public DataTable ObtenerPaquete(int idPaquete)
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Paquetes WHERE id=@id");
+            cmd.Parameters.Add("@id", SqlDbType.Int).Value = idPaquete;
+
+            return con.EjecutarSentencia(cmd);
+        }*/
+
+
+    }
+}

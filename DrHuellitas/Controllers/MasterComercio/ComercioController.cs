fb094a554fec4a0caac7593d4ce26da0efcdc9ea
GERARDO <GERARDO@DESKTOP-JE5SO9B>
2017-11-23 17:23:07 -0600

    checa el dato

--- DrHuellitas/Controllers/MasterComercio/ComercioController.cs
+++ DrHuellitas/Controllers/MasterComercio/ComercioController.cs
@@ -88,7 +88,35 @@ namespace DrHuellitas.Controllers.MasterComercio
         }
         public ActionResult chat()
         {
-            return View();
+            string modulo = "";
+            if (Session["id"] != null)
+            {
+                if ((int)Session["idtipo"] == 1)
+                {
+                    modulo = "~/Admin/Index";
+                }
+                else if ((int)Session["idtipo"] == 2)
+                {
+                    modulo = ((int)Session["status"] == 1) ? "~/Usuario/Index" : "~/Usuario/Continuar";
+                }
+                else if ((int)Session["idtipo"] == 3)
+                {
+                    if ((int)Session["status"] == 1)
+                        return View();
+                    else
+                        modulo = "~/Comercio/Continuar";
+                }
+                else if ((int)Session["idtipo"] == 4)
+                {
+                    modulo = ((int)Session["status"] == 1) ? "~/Vet/Index" : "~/Vet/Continuar";
+                }
+            }
+            else
+            {
+                modulo = "~/Inicio/Index";
+            }
+
+            return Redirect(modulo);
         }
     }
 }
